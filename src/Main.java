5ebef6f7fda3a165330e7def8c8d94c9e490dda6
Mihail Test <misha.lucenko17@gmail.com>
2024-06-15 22:02:57 +0300

    Ispravlen commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         //Цена билета
-        int Ticket_Price = 15338;
+        int ticketPrice = 15338;
 
         //Количество рублей для начисления бонусной мили
-        int Price_Mile = 20;
+        int priceMile = 20;
 
         //Расчет бонусных миль
-        int Bonus = Ticket_Price / Price_Mile;
-        System.out.println("Начислено бонусов " + Bonus);
+        int bonus = ticketPrice / priceMile;
+        System.out.println("Начислено бонусов " + bonus);
     }
-
 }
